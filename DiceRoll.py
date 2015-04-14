870fb7b410a3d6d89334445cdddaec4a9879756c
Erik Richter <erichter_h@dev.bse.edu>
2015-04-14 15:28:52 -0500

    First Round

--- DiceRoll.py
+++ DiceRoll.py
@@ -5,7 +5,28 @@ MasterDiceList = [4,6,8,10,12,20]
 PlayerOneDice = [4,6,8,10,12,20]
 PlayerTwoDice = [4,6,8,10,12,20]
 
-PlayerOneSelection = raw_input("Player One Select Dice:")
-PlayerTwoSelection = raw_input("Player Two Select Dice:")
+PlayerOneSelection = int(raw_input("Player One Select Dice:"))
+PlayerTwoSelection = int(raw_input("Player Two Select Dice:"))
 
-PlayerOneSelection
+PlayerOneRoll = randint(1,PlayerOneSelection)
+PlayerTwoRoll = randint(1,PlayerTwoSelection)
+
+PlayerOneWins = 0
+PlayerTwoWins = 0
+GamePlay = True
+
+print ("Player One: {0}, Player Two: {1}" .format(PlayerOneRoll,PlayerTwoRoll))
+
+if (PlayerOneRoll > PlayerTwoRoll):
+    print ("Player One Wins")
+	PlayerOneWins = PlayerOneWins+1
+	
+elif (PlayerOneRoll < PlayerTwoRoll):
+	print ("Player Two Wins")
+	PlayerTwoWins = PlayerTwoWins+1
+else:
+	print ("Tie Game")
+
+
+	
+
