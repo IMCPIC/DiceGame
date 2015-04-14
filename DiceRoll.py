1d8553b181b9e10d5ffe8d80f6f29bfab5ccdbe4
Erik Richter <erichter_h@dev.bse.edu>
2015-04-14 15:02:13 -0500

    First Commit

---
+++ DiceRoll.py
@@ -0,0 +1,11 @@
+import random
+from random import randint
+
+MasterDiceList = [4,6,8,10,12,20]
+PlayerOneDice = [4,6,8,10,12,20]
+PlayerTwoDice = [4,6,8,10,12,20]
+
+PlayerOneSelection = raw_input("Player One Select Dice:")
+PlayerTwoSelection = raw_input("Player Two Select Dice:")
+
+PlayerOneSelection
