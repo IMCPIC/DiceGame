fde9a29f55d258bf28d1bee12f477d57a6e4da4c
Erik Richter <erichter_h@dev.bse.edu>
2015-04-14 16:20:43 -0500

    Added Rolling 2

--- DiceRoll.py
+++ DiceRoll.py
@@ -19,29 +19,45 @@ while (GamePlay):
 	print ("Player One: {0}, Player Two: {1}" .format(PlayerOneRoll,PlayerTwoRoll))
 
 	if (PlayerOneRoll > PlayerTwoRoll):
-		if (PlayerOneRoll == 1):
+		if (PlayerOneRoll == 1):			# Player One Rolls a 1 and wins
 			GamePlay = False
 			PlayerOneWins = 2
 			print ("Player One Wins Game")
-		elif (PlayerTwoRoll == 1):
+		elif (PlayerTwoRoll == 1):			# Player Two Rolls a 1 and wins
 			GamePlay = False
 			PlayerTwoWins = 2
 			print("Player Two Wins Game")
-		else:
-			print ("Player One Wins Round")
-			PlayerOneWins = PlayerOneWins+1
+		else:								
+			if (PlayerTwoRoll == 2):		# Players Roll a 2
+				PlayerTwoWins = PlayerTwoWins-1
+			if (PlayerOneRoll == 2):
+				PlayerOneWins = PlayerOneWins-1
+			else:							#Total Gameplay wins+
+				print ("Player One Wins Round")
+				PlayerOneWins = PlayerOneWins+1
+				if (PlayerOneWins == 2):
+					GamePlay = False
+					print ("Player One Wins Game")
 	elif (PlayerOneRoll < PlayerTwoRoll):
-		if (PlayerOneRoll == 1):
+		if (PlayerOneRoll == 1): 			# Player One Rolls a 1 and wins
 			GamePlay = False
 			PlayerOneWins = 2
 			print ("Player One Wins Game")
-		elif (PlayerTwoRoll == 1):
+		elif (PlayerTwoRoll == 1): 			# Player Two Rolls a 1 and wins
 			GamePlay = False
 			PlayerTwoWins = 2
 			print ("Player Two Wins Game")
 		else:
-			print ("Player Two Wins Round")
-			PlayerTwoWins = PlayerTwoWins+1
+			if (PlayerTwoRoll == 2):		# Players Roll a 2
+				PlayerTwoWins = PlayerTwoWins-1
+			if (PlayerOneRoll == 2):
+				PlayerOneWins = PlayerOneWins-1
+			else:							#Total Gameplay wins+
+				print ("Player Two Wins Round")
+				PlayerTwoWins = PlayerTwoWins+1
+				if (PlayerTwoWins == 2):
+					GamePlay = False
+					print ("Player Two Wins Game")
 	else:
 		print ("Tie Game")
 
